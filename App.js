e6a202bd7274eedd99bbb9297a371b304610b10a
Markus Wendorf <markuswendorf@web.de>
2025-06-12 13:25:07 +0200

    repro camera resetting

--- App.js
+++ App.js
@@ -1,20 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import {
+  Camera,
+  Images,
+  MapView,
+  ShapeSource,
+  SymbolLayer,
+} from "@maplibre/maplibre-react-native";
+import { useState, useRef } from "react";
 
 export default function App() {
+  const cameraRef = useRef();
+  const [features, setFeatures] = useState({
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [12, 54],
+        },
+        properties: {},
+        id: "feature-id-1",
+      },
+    ],
+  });
+
+  const updateFeatures = () => {
+    setFeatures({
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [12, 54],
+          },
+          properties: {},
+          id: "feature-id-1",
+        },
+
+        {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [12.5, 54],
+          },
+          properties: {},
+          id: "feature-id-2",
+        },
+      ],
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MapView style={{ flex: 1 }}>
+      <Camera ref={cameraRef} />
+      <Images images={{ default: require("./assets/icon.png") }} />
+      <ShapeSource
+        shape={features}
+        onPress={(evt) => {
+          const feature = evt.features[0];
+          if (!feature) return;
+
+          cameraRef.current.setCamera({
+            zoomLevel: 7,
+            animationMode: "linearTo",
+            centerCoordinate: feature.geometry.coordinates,
+          });
+
+          // Move the map anywhere and after updating the features the camera snaps back to the setCamera call above
+          setTimeout(() => updateFeatures(), 5000);
+        }}
+      >
+        <SymbolLayer
+          id="marker-layer"
+          style={{
+            iconImage: "default",
+            iconAllowOverlap: true,
+            iconSize: 0.01,
+          }}
+        />
+      </ShapeSource>
+    </MapView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
